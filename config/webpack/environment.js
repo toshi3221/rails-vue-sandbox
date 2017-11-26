70d0567635bdb4c07fdcc499e700aca8a8231a9a
Toshiharu Takematsu <info@newfish.jp>
2017-11-26 21:08:57 +0900

    Create sample app 以下のコンポーネントエラーを参考にconfig/webpackを修正： [Rails5.1][Webpacker][Vue.js] 動かそうとするとコンポネントエラーになる問題 - Qiita https://qiita.com/taaatk/items/3137659e69b82d10cfca

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,9 @@
 const { environment } = require('@rails/webpacker')
 
-module.exports = environment
+module.exports = Object.assign({}, environment.toWebpackConfig(), {
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
+})
